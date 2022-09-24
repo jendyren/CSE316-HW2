726f0abb0f64673e1310eab89201597867ffd382
jendyren <jendyren@gmail.com>
2022-09-24 17:55:54 -0400

    Fix UI - song hyperlink, remove button, song num

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -77,7 +77,18 @@ export default class SongCard extends React.Component {
                 onDrop={this.handleDrop}
                 draggable="true"
             >
-                {song.title} by {song.artist}
+                
+                {num}.
+            <a href={"https://www.youtube.com/watch?v=" + song.youTubeId}>
+                {song.title} by {song.artist}  
+            </a>
+                                  
+            <input
+                type="button"
+                id={"delete-song-" + num}
+                className={"delete-song-button"}
+                onClick={this.handleDeleteList}
+                value={"✕"} />
             </div>
         )
     }
